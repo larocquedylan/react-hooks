0deb2295d3a7ed0d9885100d4a688981ccfaf96a
Dylan L <dylancblarocque@gmail.com>
2023-03-20 12:09:06 -0600

    useState exercise + notes

--- src/final/01.js
+++ src/final/01.js
@@ -5,9 +5,11 @@ import * as React from 'react'
 
 function Greeting() {
   const [name, setName] = React.useState('')
+
   function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form>
